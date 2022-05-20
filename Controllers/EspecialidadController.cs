4a31e2b3a65eed71c3164afe5ece83e8c7118453
Enzo.MKT1288 <enzo.dure@rieder.com.py>
2022-05-20 17:22:35 -0400

    Añadida lógica de negocio de controlador y vistas de Especialidad

--- Controllers/EspecialidadController.cs
+++ Controllers/EspecialidadController.cs
@@ -1,5 +1,7 @@
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Turnos.Models;
 
 namespace Turnos.Controllers
@@ -12,9 +14,75 @@ namespace Turnos.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(_context.Especialidad.ToList());
+            return View(await _context.Especialidad.ToListAsync());
+        }
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if( id == null) return NotFound();
+
+            var especialidad = await _context.Especialidad.FindAsync(id);
+            
+            if( especialidad == null) return NotFound();
+
+            return View(especialidad);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("IdEspecialidad,Descripcion")] Especialidad especialidad)
+        {
+            if( id != especialidad.IdEspecialidad) return BadRequest();
+            
+            if(ModelState.IsValid) //Si el bind que hicimos en los parámetreos fue correcto, esto va ser true
+            {
+                _context.Update(especialidad);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(especialidad);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if( id == null) return NotFound();
+            
+            Especialidad especialidad = await _context.Especialidad.FirstOrDefaultAsync( especialidad => especialidad.IdEspecialidad == id);
+
+            if( especialidad == null ) return NotFound();
+
+            return View(especialidad);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Especialidad especialidad = await _context.Especialidad.FindAsync(id);
+            if( especialidad == null ) return NotFound();
+
+            _context.Especialidad.Remove(especialidad);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("IdEspecialidad,Descripcion")] Especialidad especialidad)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(especialidad);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(especialidad);
         }
     }
 }
--- Models/TurnosContext.cs
+++ Models/TurnosContext.cs
@@ -11,5 +11,17 @@ namespace Turnos.Models
         }
 
         public DbSet<Especialidad> Especialidad {get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Definir manualmente los campos y propiedades de la tabla Especialidad
+            modelBuilder.Entity<Especialidad>(entidad => {
+                entidad.ToTable("Especialidad");//Indicamos el nombre de la tabla
+                entidad.HasKey(e => e.IdEspecialidad);//Indicamos el PK
+                entidad.Property(e => e.Descripcion)//Especificamos que se cree el atributo Descripcion
+                    .IsRequired()//para que no sea null
+                    .HasMaxLength(200)//máximo de caracteres
+                    .IsUnicode(false);
+            });
+        }
     }
 }
