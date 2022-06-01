367badb9dfda07e00a62cdc319011b816baa2e77
Enzo <enzod98@hotmail.com>
2022-05-31 22:42:52 -0400

    Cambios para producción

--- Startup.cs
+++ Startup.cs
@@ -47,7 +47,7 @@ namespace Turnos
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+            if( env.IsProduction() )
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
